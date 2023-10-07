38ebe4a7ef6e5b6c9bf61ec728baf9dadde9f368
joseong-ug <hoho88@naver.com>
2023-10-07 22:02:18 +0900

    Initial commit

---
+++ src/main/java/com/jobs/springsecurity/JobsSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.jobs.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobsSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jobs/springsecurity/JobsSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobs.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
